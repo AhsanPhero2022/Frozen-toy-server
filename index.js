acb2d0537c3103e7de45900cb159a8eb86854e8f
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-05-19 20:33:09 +0600

    All Toys Api added

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// toyMarket
-// mkxCiXSvbomtCofl
-console.log(process.env.DB_USER);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsh8x1y.mongodb.net/?retryWrites=true&w=majority`;
 // Create a new MongoClient
 const client = new MongoClient(uri, {
@@ -22,6 +19,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const frozenCollection = client.db("DesnyToys").collection("frozen");
+
+    app.get("/frozen", async (req, res) => {
+      const cursor = frozenCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
