85b401cbcd2e8536324e7e252bd8e82c74eeb7cc
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-05-19 16:22:46 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// toyMarket
+// mkxCiXSvbomtCofl
+
+const uri =
+  "mongodb+srv://toyMarket:mkxCiXSvbomtCofl@cluster0.xsh8x1y.mongodb.net/?retryWrites=true&w=majority";
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Assignment server is Running");
