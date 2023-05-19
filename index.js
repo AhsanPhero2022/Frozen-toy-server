767fd355bb99cb82d6a52b8cd5bcd4bbae8c0fa2
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-05-19 23:33:48 +0600

    dynamic id data pass okay

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -25,6 +25,14 @@ async function run() {
     app.get("/frozen", async (req, res) => {
       const cursor = frozenCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/frozen/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await frozenCollection.findOne(query);
       res.send(result);
     });
 
